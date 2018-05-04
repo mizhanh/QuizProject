cd916bfb904a1cf12ca968dbc2b2df779ecacb23
mizhanh <31321795+mizhanh@users.noreply.github.com>
2018-05-03 21:32:51 -0400

    cleaned up code

--- app/src/main/java/com/example/android/quizproject/MainActivity.java
+++ app/src/main/java/com/example/android/quizproject/MainActivity.java
@@ -1,18 +1,16 @@
 package com.example.android.quizproject;
 
-import android.content.Intent;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int score = 0;
-    int totalScore;
-    String name;
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,81 +20,46 @@ public class MainActivity extends AppCompatActivity {
 
     public void submitQuiz(View view) {
 
-        EditText nameInput = (EditText) findViewById(R.id.player);
-        name = nameInput.getText().toString();
+        EditText answerInput = (EditText) findViewById(R.id.answerQ2);
+        String answer = answerInput.getText().toString();
+        String correctAnswer = "Melania";
 
-        RadioButton choice1 = (RadioButton) findViewById(R.id.RadioButton1);
-        boolean isChoice1 = choice1.isChecked();
+        RadioButton choiceA = (RadioButton) findViewById(R.id.RadioButton1);
+        RadioButton choiceB = (RadioButton) findViewById(R.id.RadioButton2);
 
-        RadioButton choice2 = (RadioButton) findViewById(R.id.RadioButton2);
-        boolean isChoice2 = choice2.isChecked();
+        CheckBox q3Choice1 = (CheckBox) findViewById(R.id.Q3_Choice1);
+        CheckBox q3Choice2 = (CheckBox) findViewById(R.id.Q3_Choice2);
+        CheckBox q3Choice3 = (CheckBox) findViewById(R.id.Q3_Choice3);
+        CheckBox q3Choice4 = (CheckBox) findViewById(R.id.Q3_Choice4);
 
-        CheckBox Q2Choice1 = (CheckBox) findViewById(R.id.Q2_Choice1);
-        boolean isQ2Choice1 = Q2Choice1.isChecked();
+        CheckBox q4Choice1 = (CheckBox) findViewById(R.id.Q4_Choice1);
+        CheckBox q4Choice2 = (CheckBox) findViewById(R.id.Q4_Choice2);
+        CheckBox q4Choice3 = (CheckBox) findViewById(R.id.Q4_Choice3);
+        CheckBox q4Choice4 = (CheckBox) findViewById(R.id.Q4_Choice4);
 
-        CheckBox Q2Choice2 = (CheckBox) findViewById(R.id.Q2_Choice2);
-        boolean isQ2Choice2 = Q2Choice2.isChecked();
+        CheckBox q5Choice1 = (CheckBox) findViewById(R.id.Q5_Choice1);
+        CheckBox q5Choice2 = (CheckBox) findViewById(R.id.Q5_Choice2);
+        CheckBox q5Choice3 = (CheckBox) findViewById(R.id.Q5_Choice3);
+        CheckBox q5Choice4 = (CheckBox) findViewById(R.id.Q5_Choice4);
 
-        CheckBox Q2Choice3 = (CheckBox) findViewById(R.id.Q2_Choice3);
-        boolean isQ2Choice3 = Q2Choice3.isChecked();
+        if (choiceA.isChecked() && !choiceB.isChecked()) score += 1;
+        if (answer.equals(correctAnswer)) score +=1;
+        if (!q3Choice1.isChecked() && q3Choice2.isChecked() && q3Choice3.isChecked() && q3Choice4.isChecked()) score += 1;
+        if (q4Choice1.isChecked() && q4Choice2.isChecked() && !q4Choice3.isChecked() && !q4Choice4.isChecked()) score += 1;
+        if (!q5Choice1.isChecked() && q5Choice2.isChecked() && !q5Choice3.isChecked() && q5Choice4.isChecked()) score += 1;
 
-        CheckBox Q2Choice4 = (CheckBox) findViewById(R.id.Q2_Choice4);
-        boolean isQ2Choice4 = Q2Choice4.isChecked();
-
-        CheckBox Q3Choice1 = (CheckBox) findViewById(R.id.Q3_Choice1);
-        boolean isQ3Choice1 = Q3Choice1.isChecked();
-
-        CheckBox Q3Choice2 = (CheckBox) findViewById(R.id.Q3_Choice2);
-        boolean isQ3Choice2 = Q3Choice2.isChecked();
-
-        CheckBox Q3Choice3 = (CheckBox) findViewById(R.id.Q3_Choice3);
-        boolean isQ3Choice3 = Q3Choice3.isChecked();
-
-        CheckBox Q3Choice4 = (CheckBox) findViewById(R.id.Q3_Choice4);
-        boolean isQ3Choice4 = Q3Choice4.isChecked();
-
-        CheckBox Q4Choice1 = (CheckBox) findViewById(R.id.Q4_Choice1);
-        boolean isQ4Choice1 = Q4Choice1.isChecked();
-
-        CheckBox Q4Choice2 = (CheckBox) findViewById(R.id.Q4_Choice2);
-        boolean isQ4Choice2 = Q4Choice2.isChecked();
-
-        CheckBox Q4Choice3 = (CheckBox) findViewById(R.id.Q4_Choice3);
-        boolean isQ4Choice3 = Q4Choice3.isChecked();
-
-        CheckBox Q4Choice4 = (CheckBox) findViewById(R.id.Q4_Choice4);
-        boolean isQ4Choice4 = Q4Choice4.isChecked();
-
-        CheckBox Q5Choice1 = (CheckBox) findViewById(R.id.Q5_Choice1);
-        boolean isQ5Choice1 = Q5Choice1.isChecked();
-
-        CheckBox Q5Choice2 = (CheckBox) findViewById(R.id.Q5_Choice2);
-        boolean isQ5Choice2 = Q5Choice2.isChecked();
-
-        CheckBox Q5Choice3 = (CheckBox) findViewById(R.id.Q5_Choice3);
-        boolean isQ5Choice3 = Q5Choice3.isChecked();
-
-        CheckBox Q5Choice4 = (CheckBox) findViewById(R.id.Q5_Choice4);
-        boolean isQ5Choice4 = Q5Choice4.isChecked();
-
-        if (isChoice1) score = score + 1;
-        if (isQ2Choice3) score = score + 1;
-        if (isQ3Choice2 && isQ3Choice3 && isQ3Choice4) score = score + 1;
-        if (isQ4Choice1 && isQ4Choice2) score = score + 1;
-        if (isQ5Choice2 && isQ5Choice4) score = score + 1;
-        totalScore = score;
-        display(totalScore);
-    }
-
-    private void display(int finalScore){
-        if (totalScore >= 4) {
-            TextView scoreTextView = (TextView) findViewById(R.id.scoreView);
-            scoreTextView.setText("Awesome, " + name + "!  You do know your POTUS." + "\nScore: " + totalScore + "/5");
-        }
-
-        if (totalScore <= 2) {
-            TextView scoreTextView = (TextView) findViewById(R.id.scoreView);
-            scoreTextView.setText("You should turn on the TV!" + "\nScore: " + totalScore + "/5");
+        if (score >=3) {
+            Context context = getApplicationContext();
+            CharSequence text = "Awesome, you do know your POTUS!" + "\nScore: " + score + "/5" ;
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        } else {
+            Context context = getApplicationContext();
+            CharSequence text = "You should watch more TV!" + "\nScore: " + score + "/5" ;
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         }
     }
 
